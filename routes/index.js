1912aa2029534c6b6fbc4651091b87a3d0bb9a1a
shekhar <shekhar@excellencetechnologies.in>
2016-11-02 11:48:51 +0530

    type leave ,hello , help in hrbot and see response

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 require('node-import');
 imports('config/index');
-console.log(process.env.SLACK_API_TOKEN)
 
 var RtmClient = require('@slack/client').RtmClient;
 var MemoryDataStore = require('@slack/client').MemoryDataStore;
@@ -21,31 +20,33 @@ rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function () {
     console.log('Connected to ' + team.name + ' as ' + user.name);
 });
 
-// var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
-// // Responds to a message with a 'hello' DM
-// rtm.on(RTM_EVENTS.MESSAGE, function (message) {
-//     var user = rtm.dataStore.getUserById(message.user)
-//     var dm = rtm.dataStore.getDMByName(user.name);
-//     rtm.sendMessage('Hello ' + user.name + '!', dm.id);
-// });
+var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+// Responds to a message with a 'hello' DM
+rtm.on(RTM_EVENTS.MESSAGE, function (message) {
+    var user = rtm.dataStore.getUserById(message.user)
+    var dm = rtm.dataStore.getDMByName(user.name);
+    if (message.text == 'leave' || message.text == 'hello' || message.text == 'help' || message.text == 'hi' || message.text == 'helo' || message.text == 'hey') {
+        if (message.text == 'leave') {
+            rtm.sendMessage('you are not apply leave ' + user.name + '!', dm.id);
+        } else if (message.text == 'hello' || message.text == 'hi' || message.text == 'helo' || message.text == 'hey') {
+            rtm.sendMessage('hello ' + user.name + '!', dm.id);
+        } else if (message.text == 'help') {
+            rtm.sendMessage('These are the different options for you: \n 1. leave', dm.id);
+        } else {
+            rtm.sendMessage('invalid data' + user.name + '!', dm.id);
+        }
+    } else {
+        rtm.sendMessage("I don't understand" + " " + message.text + ". " + "Please use 'help' to see all options" + '.', dm.id);
+    }
+});
 
-//////////////////////use for sending message //////////////////////////
+//////////////////////when app is started it sends a mesage to shekhar  //////////////////////////
 var RTM_CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS.RTM;
 // you need to wait for the client to fully connect before you can send messages
 rtm.on(RTM_CLIENT_EVENTS.RTM_CONNECTION_OPENED, function () {
-    // This will send the message 'this is a test message' to the channel identified by id 'C0CHZA86Q'
-    rtm.sendMessage('text : this is a test message ,\n username: shekhar, \n mrkdwn :true', config.shekhar_channelId, function messageSent() {
-        // optionally, you can supply a callback to execute once the message has been sent
+// This will send the message 'this is a test message' to the channel identified by id 'C0CHZA86Q'
+    rtm.sendMessage('text : welcome,\n username: shekhar, \n your app is running now', config.shekhar_channelId, function messageSent() {
     });
-});
-
-
-var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
-rtm.on(RTM_EVENTS.MESSAGE, function (message) {
-    // Listens to all `message` events from the team
-});
-rtm.on(RTM_EVENTS.CHANNEL_CREATED, function (message) {
-    // Listens to all `channel_created` events from the team
 });
 
 module.exports = router;
